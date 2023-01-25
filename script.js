62280543f2c09e96ea10b90c843d034371e9e744
Richardy <richardyleonardoreis@gmail.com>
2023-01-24 21:56:12 -0300

    função adicionada

---
+++ script.js
@@ -0,0 +1,22 @@
+let sliderElement       = document.querySelector("#slider");
+let password            = document.querySelector("#password");
+let containerPassword   = document.querySelector("#container-password");
+let sizePassword        = document.querySelector("#valor");
+
+sizePassword.innerHTML  = sliderElement.value;
+
+//fonte da senha randômica
+let charset ='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#';
+let novaSenha = '';
+
+sliderElement.oninput = function() {
+    sizePassword.innerHTML = this.value;
+}
+
+function generatePassword() {
+    let pass = '';
+    for(let i = 0, n = charset.length; i < sliderElement.value; i++) {
+        pass += charset.charAt(Math.floor(Math.random() * n));
+    }
+    console.log(pass);
+}
